082b18f5d791f119b813bcd842e0987de661a36b
davi lindoso <davi.lindoso@accenture.com>
2018-11-12 10:59:05 -0300

    adicionando list views para opcoes alunos e exercicios.

---
+++ app/src/main/java/com/example/davilindoso/personalapp/AlunosFragment.kt
@@ -0,0 +1,15 @@
+package com.example.davilindoso.personalapp
+
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class AlunosFragment : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return LayoutInflater.from(container?.context).inflate(R.layout.alunos_layout, container,false)
+    }
+
+}
