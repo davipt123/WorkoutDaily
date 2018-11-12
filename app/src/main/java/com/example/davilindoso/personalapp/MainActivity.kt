4a8bf16b7e01adf30f7f840d32a2fd1464715142
davi lindoso <davi.lindoso@accenture.com>
2018-11-12 08:10:58 -0300

    nome de usuario e email no header do menu

--- app/src/main/java/com/example/davilindoso/personalapp/MainActivity.kt
+++ app/src/main/java/com/example/davilindoso/personalapp/MainActivity.kt
@@ -8,15 +8,27 @@ import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.app_bar_main.*
 
 class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
+    private lateinit var txtNome: TextView
+    private lateinit var auth: FirebaseAuth
+    private var user: FirebaseUser? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+        recuperarDadosUsuario()
 
         fab.setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -30,6 +42,38 @@ class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelecte
         toggle.syncState()
 
         nav_view.setNavigationItemSelectedListener(this)
+    }
+
+    private fun recuperarDadosUsuario(){
+        auth = FirebaseAuth.getInstance()
+        user = auth.currentUser
+
+        var displayName = user!!.displayName
+        var emailUser = user!!.email
+        var profileUri = user!!.photoUrl
+        user!!.providerData.forEach() {
+            if (displayName == null && it.displayName != null) {
+                displayName = it.displayName
+            }
+            if (profileUri == null && it.photoUrl != null) {
+                profileUri = it.photoUrl
+            }
+        }
+        if (displayName != null) {
+            val navigationView:NavigationView? = findViewById(R.id.nav_view);
+            val hView:View = navigationView!!.getHeaderView(0);
+            val nav_user:TextView = hView.findViewById(R.id.userId);
+            nav_user.setText(displayName)
+        }else{
+            Toast.makeText(this,"Não foi possível recuperar dados do Usuário",Toast.LENGTH_LONG)
+        }
+
+        if(emailUser != null){
+            val navigationView:NavigationView? = findViewById(R.id.nav_view);
+            val hView:View = navigationView!!.getHeaderView(0);
+            val nav_user:TextView = hView.findViewById(R.id.userEmail);
+            nav_user.setText(emailUser)
+        }
     }
 
     override fun onBackPressed() {
